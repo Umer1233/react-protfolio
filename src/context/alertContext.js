f7366d8c28a99b014cfae90d8d99f9114553a354
Umer1233 <umerullahazeemi@gmail.com>
2024-08-01 13:16:39 +0500

    Push 2

--- src/context/alertContext.js
+++ src/context/alertContext.js
@@ -6,7 +6,7 @@ export const AlertProvider = ({ children }) => {
   const [state, setState] = useState({
     isOpen: false,
     // Type can be either "success" or "error"
-    type: 'successs',
+    type: 'succes',
     // Message to be displayed, can be any string
     message: '',
   });
